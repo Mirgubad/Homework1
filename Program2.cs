cdc7cced136a44d66cf533b66c31b6e80fc7d630
Mirgubad <104448453+Mirgubad@users.noreply.github.com>
2022-05-13 13:31:41 +0400

    CalcAvg

---
+++ Program2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            CalcAvg(58);
+            Console.ReadLine();
+        }
+
+
+
+
+
+        static void CalcAvg(int score)
+        {
+
+            int[] examscore = { 60, 70, 80, 90, 100 };
+            foreach (int i in examscore)
+            {
+                if (score > i)
+                {
+
+                    Console.WriteLine("Mezun oldunuz");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Mezun ola bilmediniz");
+                    break;
+                }
+            }
+
+        }
+
+
+
+
+
+    }
+}
