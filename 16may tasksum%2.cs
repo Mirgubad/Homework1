e7b85be5885fcd284ad918e67eace2ad89813eac
Mirgubad <104448453+Mirgubad@users.noreply.github.com>
2022-05-14 20:42:32 +0400

    16may tasksum%2

---
+++ 16may tasksum%2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = { 1, 2, 3, 4, 8 };
+
+
+            Console.WriteLine(ArrSum(arr1));
+
+
+        }
+
+        static string ArrSum(int[] arr)
+        {
+            int sum = 0;
+
+            foreach (int i in arr)
+            {
+                sum += i;
+
+
+
+            }
+            if (sum % 2 == 0)
+            {
+
+                return "Ededlerin cemi 2 e bolunur:";
+            }
+
+
+            else
+            {
+            }
+            return "Ededlerin cemi 2 e bolunmur:";
+        }
+
+
+
+
+
+    }
+}
