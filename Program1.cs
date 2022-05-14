2747cac7b2eb1acabc18b7a8c4dc5296e337a53d
Mirgubad <104448453+Mirgubad@users.noreply.github.com>
2022-05-14 20:44:23 +0400

    task IsPrime

---
+++ Program1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main()
+        {
+            IsPrime(13);
+
+            Console.ReadLine();
+        }
+
+
+
+        static void IsPrime(int n)
+        {
+
+            int i = 0;
+            int m = 0;
+            int counter = 0;
+
+
+
+            m = n / 2;
+            for (i = 2; i <= m; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.WriteLine("False");
+                    counter++;
+                    break;
+                }
+            }
+            if (counter == 0)
+                Console.WriteLine("True");
+
+        }
+    }
+
+
+
+}
+
+
+
+
