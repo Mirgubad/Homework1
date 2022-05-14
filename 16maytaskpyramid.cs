b6c2dbb0d1f4ba1883d70b4d5a9901a0b2e53715
Mirgubad <104448453+Mirgubad@users.noreply.github.com>
2022-05-14 20:42:57 +0400

    taskPyramid

---
+++ 16maytaskpyramid.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pyramid(6, 6);
+        
+        }
+
+
+        static void Pyramid(int en,int hundurluk)
+        {
+            
+            
+
+                for (int i = 0; i < en; i++)
+                {
+                    for (int j = 0; j < hundurluk; j++)
+                    {
+                        if (i < j)
+                        {
+
+                            break;
+                        }
+                        Console.Write('*');
+
+                    }
+                    Console.WriteLine(' ');
+
+                }
+
+           
+           
+
+        }
+    
+    
+    
+    
+    
+    
+    
+   
+    
+    }
+}
