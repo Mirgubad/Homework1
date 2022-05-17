d351dabedd386bd5a21d30482eebb21ca7759747
Mirgubad <104448453+Mirgubad@users.noreply.github.com>
2022-05-17 13:34:34 +0400

    17 may task

---
+++ StringReverse.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main()
+        {
+            System.Console.WriteLine(StringReverse("Nurlan Faiq Emil Tural Yusif"));
+
+        }
+
+        static string StringReverse(string name)
+        {
+            string revname = "";
+
+            for (int i = name.Length - 1; i >= 0; i--)
+            {
+                revname += name[i];
+            }
+            return revname;
+
+        }
+
+
+
+
+    }
+}
