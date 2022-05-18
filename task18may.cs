9f98296725b3bbe75d0095c68b2105b28e7f2557
Mirgubad <104448453+Mirgubad@users.noreply.github.com>
2022-05-19 00:10:51 +0400

    18 may task

---
+++ task18may.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ConsoleApp21
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[] arr1 = { 2, 3, 4, 7 };
+            int[] arr2 = { 2, 4, 7, 8 };
+
+            Console.WriteLine(İsArrayContains(arr1, arr2));
+
+        }
+
+        static bool İsArrayContains(int[] arr1, int[] arr2)
+        {
+
+            int a = 0;
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                for (int j = 0; j < arr2.Length; j++)
+
+
+                {
+                    if (arr1[i] == arr2[j])
+                    {
+                        a++;
+                    }
+                    if (a == arr2.Length && IsSorted(arr2))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+
+        }
+
+        static bool IsSorted(int[] a)
+        {
+            for (int i = 0; i < a.Length - 1; ++i)
+            {
+                if (a[i].CompareTo(a[i + 1]) > 0)
+                    return false;
+            }
+            return true;
+        }
+
+    }
+
+}
+
