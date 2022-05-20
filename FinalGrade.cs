2acc5146121fb14e5909ba03725238efb1e3ef5e
Mirgubad <104448453+Mirgubad@users.noreply.github.com>
2022-05-20 22:01:50 +0400

    23 may tasks

---
+++ FinalGrade.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ConsoleApp28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student myscore = new Student
+            {
+                HomeWorkGrades = 90,
+                finalgrade = 90,
+                ishtiraketdiyi = 1,
+                umumiders = 8,
+                ProjectGrades = 83,
+                Davamiyyet = true,
+            };
+            myscore.FinalGrades(100);
+        }
+
+
+    }
+    class Student
+    {
+        public int HomeWorkGrades;
+        public int ProjectGrades;
+        public int finalgrade;
+        public bool Davamiyyet;
+        public int umumiders;
+        public int ishtiraketdiyi;
+
+        public void FinalGrades(int umumibal)
+        {
+            double score = (ProjectGrades * 0.25) + (HomeWorkGrades * 0.25) + (finalgrade * 0.4) + (ishtiraketdiyi * 0.1);
+
+            if (score >= 90)
+            {
+                Console.WriteLine("HighHonour");
+            }
+            else if (score > 80)
+            {
+                Console.WriteLine("Honour");
+            }
+            else
+            {
+                Console.WriteLine("Normal");
+            }
+        }
+    }
+
+
+
+
+}
---
+++ IfCharContains.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = { "Badam", "Natiq", "Eli" };
+            char user = 'i';
+            StringPrint(arr, user);
+        }
+
+
+        static void StringPrint(string[] arr, char letter)
+        {
+            foreach (var item in arr)
+            {
+                for (int j = 0; j < item.Length; j++)
+                {
+                    if (item[j] == letter)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+            }
+
+        }
+    }
+}
+
