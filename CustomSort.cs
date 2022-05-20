7859c5679fe81201ca5695d64844ec182cc67134
Mirgubad <104448453+Mirgubad@users.noreply.github.com>
2022-05-20 22:07:26 +0400

    23 may tasks

---
+++ CustomSort.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace ConsoleApp29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 5, 4, 8, 11, 21, 1 };
+            ArraySort(arr);
+        }
+        public static void ArraySort(int[] arr)
+        {
+            int length = arr.Length;
+            for (int i = 0; i < length - 1; i++)
+            {
+                if (arr[i] < arr[i + 1])
+                {
+
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+
+                    i = -1;
+                }
+            }
+            foreach (int item in arr)
+            {
+                Console.WriteLine(item);
+
+
+            }
+        }
+
+
+
+    }
+}
+
+
